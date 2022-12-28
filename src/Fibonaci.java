d56b08500878073120c6170e0e7dbc21d926a299
ganesh <ganeshsadhashivam@outlook.com>
2022-12-28 16:07:58 +0530

    using Instant class Duration class fromm java 8 to measure the performance

--- src/Fibonaci.java
+++ src/Fibonaci.java
@@ -1,8 +1,21 @@
+import java.time.Duration;
+import java.time.Instant;
+
 public class Fibonaci{
     public static void main(String[] args) {
 
-        System.out.println(fibonaciRecursive(10));
+
+    for(int i=10;i<=50;i+=10){
+
+        final Instant start = Instant.now();
+        System.out.printf("fibonaci(%d)=%d%n",i,fibonaciRecursive(i));
+        final long timeElapsed = Duration.between(start,Instant.now()).toMillis();
+        System.out.printf("time taken %d ms%n%n", timeElapsed);
     }
+    }
+
+    //Time complexity
+    //O(2^n)
 
     private static long fibonaciRecursive(final int n){
         //F(n) = F(n-1) + F(n-2)
